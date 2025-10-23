1fccf6acac55a5da55eb22b36ac956c52d3b5a1f
anandita-3217 <dakshugarimella@gmail.com>
2025-10-24 00:53:34 +0530

    Happy Birthday!

---
+++ src/BirthdayWishes/BirthdayWishes.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import cake from './assets/cake.gif';
+const wishes = ["HBD loser!","Hope you get everytthing you want!","U the best!","I💖U","Youre half way to 50!","Hope your day is amazing!"];
+function BirthdayWishes(){
+    const [wish,setWish] = useState(''); 
+    const [celebrate,setCelebrate] = useState(false);
+    const handleWish = () => {
+        const randomWish = wishes[Math.floor(Math.random()*wishes.length)];
+        setWish(randomWish);
+        setCelebrate(true);
+    }
+    const age =  Math.abs (2000 - new Date().getFullYear());
+    return (
+        <div>
+            <header className="App-header">Happy {age}th Birthday Ananya!
+                <img src={cake} alt="cake"/>
+                <button className="btn" onClick={handleWish} >Make a Wish</button>
+                <p>💫{wish}💫</p>
+            </header>
+        </div>
+    )
+}
+export default BirthdayWishes;
